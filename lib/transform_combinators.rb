048e712124fd128c831b8a1ba749c63b5148b8c1
Martin Chabot <chabotm@gmail.com>
2017-07-07 17:08:14 -0400

    removing tabls

--- lib/transform_combinators.rb
+++ lib/transform_combinators.rb
@@ -4,11 +4,11 @@ class Module
   def def_fnc(*syms)
     syms.each do |sym|
       raise NameError.new("invalid function name: #{sym}") unless /\A[_A-Za-z]\w*\z/ =~ sym
-			class_eval(<<-EOS, __FILE__, __LINE__ + 1)
-					def #{sym}
-						@@#{sym}
-					end
-			EOS
+      class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+        def #{sym}
+            @@#{sym}
+        end
+      EOS
     end
   end
 end
@@ -16,12 +16,12 @@ end
 module TransformCombinators
   def_fnc :same, :hash_of, :array_of, :default, :scalar
 
-	@@same = -> a { a }
-	@@hash_of = -> fields , hash { 
-		hash ||= {}
-		fields.map { |(key, fn)| [key, fn.(hash[key])] }.to_h
-	}.curry
-	@@array_of = -> fn, value { value.kind_of?(Array) ?  value.map(&fn) : [] }.curry
-	@@default = -> default, a { a.nil? ? default : a  }.curry
-	@@scalar = -> a { a.kind_of?(Array) || a.kind_of?(Hash) ? nil : a }
+  @@same = -> a { a }
+  @@hash_of = -> fields , hash { 
+    hash ||= {}
+    fields.map { |(key, fn)| [key, fn.(hash[key])] }.to_h
+  }.curry
+  @@array_of = -> fn, value { value.kind_of?(Array) ?  value.map(&fn) : [] }.curry
+  @@default = -> default, a { a.nil? ? default : a  }.curry
+  @@scalar = -> a { a.kind_of?(Array) || a.kind_of?(Hash) ? nil : a }
 end
