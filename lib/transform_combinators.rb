35d1e02dbcc5a114e5ca57054fca062ec6b6f036
Martin Chabot <chabotm@gmail.com>
2017-07-07 09:29:42 -0400

    transform combinator

--- lib/transform_combinators.rb
+++ lib/transform_combinators.rb
@@ -1,5 +1,27 @@
 require "transform_combinators/version"
 
+class Module
+  def def_fnc(*syms)
+    syms.each do |sym|
+      raise NameError.new("invalid function name: #{sym}") unless /\A[_A-Za-z]\w*\z/ =~ sym
+			class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+					def #{sym}
+						@@#{sym}
+					end
+			EOS
+    end
+  end
+end
+
 module TransformCombinators
-  # Your code goes here...
+  def_fnc :same, :hash_of, :array_of, :default, :scalar
+
+	@@same = -> a { a }
+	@@hash_of = -> fields , hash { 
+		hash ||= {}
+		fields.map { |(key, fn)| [key, fn.(hash[key])] }.to_h
+	}.curry
+	@@array_of = -> fn, value { value.kind_of?(Array) ?  value.map(&fn) : [] }.curry
+	@@default = -> default, a { a.nil? ? default : a  }.curry
+	@@scalar = -> a { a.kind_of?(Array) || a.kind_of?(Hash) ? nil : a }
 end
--- test/transform_combinators_test.rb
+++ test/transform_combinators_test.rb
@@ -1,11 +1,38 @@
 require "test_helper"
 
 class TransformCombinatorsTest < Minitest::Test
+  include TransformCombinators
+
   def test_that_it_has_a_version_number
     refute_nil ::TransformCombinators::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true 
+  def test_same 
+    assert_equal 12, same.(12)
+  end
+
+  def test_hash_of
+    user = {name: "Martin", age: 23}
+    assert_equal({name: "Martin"}, hash_of.({name: same}).(user))
+  end
+
+  def test_scalar_return_value_if_scalar
+    assert_equal(12, scalar.(12))
+  end
+
+  def test_scalar_returns_nil_if_hash
+    assert_nil scalar.({})
+  end
+
+  def test_scalar_returns_nil_if_array
+    assert_nil scalar.([])
+  end
+
+  def test_default_returns_default_value_on_nil
+    assert_equal 12, default.(12).(nil) 
+  end
+
+  def test_default_returns_the_input_value_when_not_nil
+    assert_equal 10, default.(12).(10) 
   end
 end
